2c29ac742a274d6710e8c057f8f5d10f1831e3ac
Luukaah <59238203+Luukaah@users.noreply.github.com>
2020-08-18 00:49:26 -0300

    Add project files.

---
+++ Estrutura_Condicional/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_Condicional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF AND ELSE , OPERADORES CONDICIONAIS ;
+
+            int x = 10;
+          
+            Console.WriteLine(" ENTRE COM UM NUMERO INTEIRO ");
+
+           x = int.Parse(Console.ReadLine()); // CONVERTENDO INT EM STRING PARA PODER IMPRIMIR NA TELA ;
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("PAR");
+
+                }
+
+            else
+            {
+                Console.WriteLine("IMPAR");
+            }
+
+
+
+            }
+
+        }
+    }
